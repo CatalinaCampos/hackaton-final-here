2f8c25447467bd8df6e97d6ddaeb53e7582578b9
olgamperez <ing.olgaperez@gmail.com>
2019-05-22 13:49:36 -0400

    Creando componente de notificaciones

---
+++ src/components/Notification.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Notification extends Component{
+    render(){
+        return(
+            <div >
+            <nav>
+                <p>NOTIFICACIONES</p>
+            </nav>
+            </div>
+        )
+    }
+}
+export default Notification;
