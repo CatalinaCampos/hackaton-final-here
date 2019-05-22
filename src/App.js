d03ee9fc57e9911839dcd56e166cde3c212ce152
olgamperez <ing.olgaperez@gmail.com>
2019-05-22 10:08:03 -0400

    reestructuracion

--- src/App.js
+++ src/App.js
@@ -1,47 +1,106 @@
-// import React from 'react';
 import React, { Component } from 'react';
-import './App.css';
-import ThemeSelector from './components/ThemeSelector';
 import Map from './components/Map';
+import SearchDirection from './components/Search'
+
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            theme: 'normal.day',
-            latitude: false,
-            longitude: false,
-            error: null,
-        }
+ constructor(props) {
+   super(props);
 
-        this.onChange = this.onChange.bind(this);
+   this.state = {
+    app_id:"68jKP099OUN8oLgbd5Jq",
+    app_code:"Rb8T_A8l4p9bEPt2eawMCA",
+     startPoint: {
+       lat: '',
+       long: '',
+     },
+     endPoint: {
+       lat: '',
+       long: '',
+     },
 
-    }
+   }
 
-    onChange(evt) {
-        evt.preventDefault();
-        var change = evt.target.id;
-        this.setState({
-          ...this.state,
-            "theme": change,
-        });
-    }
+   this.functionCoordUser = this.functionCoordUser.bind(this);
 
-   
-    render() {
-        return (
-            <div className="App">
-                <Map
-                    app_id="68jKP099OUN8oLgbd5Jq"
-                    app_code="Rb8T_A8l4p9bEPt2eawMCA"
-                    zoom="12"
-                    theme={ this.state.theme }
-                />
-                <ThemeSelector changeTheme={ this.onChange } />
-                
-            </div>
-        );
-    }
+ }
+
+ componentDidMount() {
+   if (navigator.geolocation) {
+     console.log(navigator.geolocation)
+     navigator.geolocation.getCurrentPosition(
+       (position) => {
+         this.setState({
+           ...this.state,
+           lat: position.coords.latitude,
+           long: position.coords.longitude,
+           error: null,
+         });
+       },
+       (error) => {
+         // alert(error.message)
+         this.setState({
+           ...this.state,
+           error: error.message
+         })
+       }
+     );
+   }
+ }
+
+ async functionCoordUser (start, end) {
+   this.startPoint = await fetch("https://geocoder.api.here.com/6.2/geocode.json?app_id=" + this.state.app_id + "&app_code=" + this.state.app_code + "&searchtext=" + start)
+     .then(data => data.json())
+     .then(data => {
+       return {
+         lat: data.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude,
+         long: data.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude,
+         address: data.Response.View[0].Result[0].Location.Address.Label,
+       }
+     })
+   this.endPoint = await fetch("https://geocoder.api.here.com/6.2/geocode.json?app_id=" + this.state.app_id + "&app_code=" + this.state.app_code + "&searchtext=" + end)
+     .then(data => data.json())
+     .then(data => {
+       return {
+         lat: data.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude,
+         long: data.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude,
+         address: data.Response.View[0].Result[0].Location.Address.Label,
+       }
+     })
+
+   this.setState({
+     ...this.state,
+     startPoint: {
+       ...this.startPoint
+
+     },
+     endPoint: {
+       ...this.endPoint
+
+     },
+
+   })
+
+ }
+
+ render() {
+   return (
+     <div className="App">
+       <Map
+         app_id={this.state.app_id}
+         app_code={this.state.app_code}
+         lat="-33.4489"
+         lng="-70.6693"
+         zoom="13"
+         startPoint={this.state.startPoint}
+         endPoint={this.state.endPoint}
+
+       />
+      <SearchDirection coordUser={this.functionCoordUser}/>
+
+     </div>
+   );
+ }
 }
 
 export default App;
