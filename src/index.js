64319c649c71c17d57c0896a50a25a35a6fe8d1c
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-05-23 14:58:01 -0400

    Deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 import Loading from './components/Loading/Loading'
 // import Profile from './components/Profile/Profile';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
