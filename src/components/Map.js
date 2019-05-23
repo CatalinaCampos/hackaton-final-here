a614a60ef6dbce3454ddca3e5bccbe9977e9159e
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-05-23 19:57:35 -0400

    Eventos en mapa

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { faGrimace } from '@fortawesome/free-solid-svg-icons';
 import { faAdobe } from '@fortawesome/free-brands-svg-icons';
+
 // import { Marker } from 'react-leaflet';
 
 export default class Map extends React.Component {
@@ -20,7 +21,6 @@ export default class Map extends React.Component {
             },
             zoom: props.zoom,
         }
-
     }
 
     componentDidMount() {
@@ -40,43 +40,60 @@ export default class Map extends React.Component {
         // eslint-disable-next-line
         var ui = new window.H.ui.UI.createDefault(this.map, layer)
 
-
-
+        var pngIcon = new window.H.map.Icon("https://i.imgur.com/MbZ6QdK.png", {size: {w: 50, h: 45}})
+        var pngIcon1 = new window.H.map.Icon("https://i.imgur.com/8y9vT3N.png", {size: {w: 50, h: 45}})
+        var pngIcon2 = new window.H.map.Icon("https://i.imgur.com/a8XYtio.png", {size: {w: 50, h: 45}})
         // Create a marker for the point:
 
-        this.startMarker1 = new window.H.map.Marker({
-            lat: -33.4171066,
-            lng: -70.6395131,
+        this.startMarker = new window.H.map.Marker({
+            lat: -33.41907,
+            lng: -70.641816,
+        }, {
+                icon: pngIcon
+            });
+
+        this.startMarker1 = new window.H.map.Marker({ /* Museo de muebles */
+            lat: -33.39471,
+            lng: -70.64264
+        }, {
+            icon: pngIcon1
         });
 
-        this.startMarker2 = new window.H.map.Marker({
-            lat: -33.4177695,
-            lng: -70.6507466,
-
+        this.startMarker2 = new window.H.map.Marker({ /* Heroinas */ 
+            lat: -33.417769,
+            lng: -70.650747
+        }, {
+            icon: pngIcon1
         });
 
-        this.startMarker3 = new window.H.map.Marker({
-            lat: -18.4716837,
-            lng: -70.2846041,
+        this.startMarker3 = new window.H.map.Marker({ /* La chascona PAGADO*/
+            lat: -33.43107,
+            lng: -70.60454
+        }, {
+            icon: pngIcon2
         });
 
-        this.startMarker4 = new window.H.map.Marker({
-            lat: -33.4262439,
-            lng: -70.6507595,
+        this.startMarker4 = new window.H.map.Marker({ /* Cementerio  */
+            lat: -33.4162901,
+            lng: -70.6483638,
+        }, {
+            icon: pngIcon1
         });
 
-        this.startMarker5 = new window.H.map.Marker({
-            lat: -33.4140072,
-            lng: -70.6436514,
+        this.startMarker5 = new window.H.map.Marker({ /* Recorrido cementerio */
+            lat: -33.3991974,
+            lng: -70.6428294,
+        }, {
+            icon: pngIcon1
         });
 
-        this.endMarker1 = new window.H.map.Marker({
+        /*this.endMarker1 = new window.H.map.Marker({
             lat: -33.4190702,
             lng: -70.6418162
-        });
+        });*/
 
 
-        this.map.addObjects([this.startMarker1, this.startMarker2, this.startMarker3, this.startMarker4, this.startMarker5, this.endMarker1 ])
+        this.map.addObjects([this.startMarker, this.startMarker1, this.startMarker2, this.startMarker3, this.startMarker4, this.startMarker5 /*this.endMarker1*/])
     }
 
     componentDidUpdate() {
@@ -96,68 +113,68 @@ export default class Map extends React.Component {
         }
         console.log("fuera de los if");
 
-    this.onResult = result => {
-        console.log(result)
-        var route,
-            routeShape,
-            startPoint,
-            endPoint,
-            linestring;
-        if (result.response && result.response.route) {
-            // Pick the first route from the response:
-            route = result.response.route[0];
-            // Pick the route's shape:
-            routeShape = route.shape;
-            // Create a linestring to use as a point source for the route line
-            linestring = new window.H.geo.LineString();
-            // Push all the points in the shape into the linestring:
-            routeShape.forEach(function (point) {
-                var parts = point.split(',');
-                linestring.pushLatLngAlt(parts[0], parts[1]);
-            });
-            // Retrieve the mapped positions of the requested waypoints:
-            startPoint = route.waypoint[0].mappedPosition;
-            endPoint = route.waypoint[1].mappedPosition;
+        this.onResult = result => {
+            console.log(result)
+            var route,
+                routeShape,
+                startPoint,
+                endPoint,
+                linestring;
+            if (result.response && result.response.route) {
+                // Pick the first route from the response:
+                route = result.response.route[0];
+                // Pick the route's shape:
+                routeShape = route.shape;
+                // Create a linestring to use as a point source for the route line
+                linestring = new window.H.geo.LineString();
+                // Push all the points in the shape into the linestring:
+                routeShape.forEach(function (point) {
+                    var parts = point.split(',');
+                    linestring.pushLatLngAlt(parts[0], parts[1]);
+                });
+                // Retrieve the mapped positions of the requested waypoints:
+                startPoint = route.waypoint[0].mappedPosition;
+                endPoint = route.waypoint[1].mappedPosition;
 
-        this.startMarker = new window.H.map.Marker({
-            lat: startPoint.latitude,
-            lng: startPoint.longitude,
-        });
-        this.endMarker = new window.H.map.Marker({
-            lat: endPoint.latitude,
-            lng: endPoint.longitude,
-        });
-        // Create a polyline to display the route:
-        this.routeLine = new window.H.map.Polyline(linestring, {
-            style: {
-                strokeColor: 'blue',
-                lineWidth: 2
+                this.startMarker = new window.H.map.Marker({
+                    lat: startPoint.latitude,
+                    lng: startPoint.longitude,
+                });
+                this.endMarker = new window.H.map.Marker({
+                    lat: endPoint.latitude,
+                    lng: endPoint.longitude,
+                });
+                // Create a polyline to display the route:
+                this.routeLine = new window.H.map.Polyline(linestring, {
+                    style: {
+                        strokeColor: 'blue',
+                        lineWidth: 2
+                    }
+                });
+
+                // Add the route polyline and the two markers to the map:
+                this.map.addObjects([this.routeLine, this.startMarker, this.endMarker]);
+                // Set the map's viewport to make the whole route visible:
+                this.map.setViewBounds(this.routeLine.getBounds());
             }
-        });
-
-            // Add the route polyline and the two markers to the map:
-            this.map.addObjects([this.routeLine, this.startMarker, this.endMarker]);
-            // Set the map's viewport to make the whole route visible:
-            this.map.setViewBounds(this.routeLine.getBounds());
-        }
-    };
+        };
 
         // Get an instance of the routing service:
         this.router = this.platform.getRoutingService();
 
-    // Call calculateRoute() with the routing parameters,
-    // the callback and an error callback function (called if a
-    // communication error occurs):
-    this.router.calculateRoute(this.routingParameters, this.onResult,
-        function (error) {
-            alert(error.message);
-        });
+        // Call calculateRoute() with the routing parameters,
+        // the callback and an error callback function (called if a
+        // communication error occurs):
+        this.router.calculateRoute(this.routingParameters, this.onResult,
+            function (error) {
+                alert(error.message);
+            });
 
     }
 
     render() {
-        return ( <div ref = "here-map"
-            style = {
+        return (<div ref="here-map"
+            style={
                 {
                     width: '100%',
                     height: '570px',
@@ -165,7 +182,7 @@ export default class Map extends React.Component {
                     marginTop: '8%'
                 }
             } >
-            </div>
+        </div>
         );
     }
 }
